8c30d13140f3ff70813813112426e565a21426bc
qjmoney <qjthought@gmail.com>
2023-11-19 02:47:41 +0900

    feat: Implement get_sympathy_users

--- projects/qjmoney/231119_blogcrawler/tests/test_blogcrawler.py
+++ projects/qjmoney/231119_blogcrawler/tests/test_blogcrawler.py
@@ -1,5 +1,11 @@
+from blogcrawler.blogcrawler import get_sympathy_users
 
 
 def test_get_sympathy_users():
     assert isinstance(get_sympathy_users("qjmoney", "223268359372"), list)
     assert len(get_sympathy_users("qjmoney", "223268359372")) > 40
+
+
+def test_get_sympathy_users_more_than_itemcount():
+    assert isinstance(get_sympathy_users("qjmoney", "223184189607"), list)
+    assert len(get_sympathy_users("qjmoney", "223184189607")) > 60
