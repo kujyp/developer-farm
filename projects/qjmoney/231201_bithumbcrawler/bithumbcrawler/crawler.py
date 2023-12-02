76781f72e4de83f0a0e6ab92f60dcc51019404f0
qjmoney <qjthought@gmail.com>
2023-12-02 15:54:48 +0900

    feat: Implement get_market_notices (qjmoney#1)

--- projects/qjmoney/231201_bithumbcrawler/bithumbcrawler/crawler.py
+++ projects/qjmoney/231201_bithumbcrawler/bithumbcrawler/crawler.py
@@ -1,8 +1,32 @@
 import requests
+from bs4 import BeautifulSoup
+from lxml import etree
 
-def get_notices():
 
+def get_market_notices():
     url = "https://cafe.bithumb.com/view/boards/43"
-    response = requests.get(url)
 
-    return []
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "sec-ch-ua": "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-site",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    dom = etree.HTML(str(soup))
+    targets = dom.xpath('//*[@id="dataTables"]/tbody/tr[*]/td[2]/a')
+    ret = []
+    for target in targets:
+        if "[마켓 추가]" in target.text:
+            ret.append(target.text)
+
+    return ret
--- projects/qjmoney/231201_bithumbcrawler/tests/test_crawler.py
+++ projects/qjmoney/231201_bithumbcrawler/tests/test_crawler.py
@@ -1,7 +1,11 @@
-from bithumbcrawler.crawler import get_notices
+from bithumbcrawler.crawler import get_market_notices
 
 
-def test_get_notices():
-    actual = get_notices()
+def test_get_market_notices():
+    actual = get_market_notices()
 
-    assert len(actual) > 0
+    assert len(actual) == 4
+    assert any(["빅타임" in each for each in actual])
+    assert any(["모스코인" in each for each in actual])
+    assert any(["홋스퍼" in each for each in actual])
+    assert any(["하이파이" in each for each in actual])
